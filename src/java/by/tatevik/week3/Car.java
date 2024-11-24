86f18f7ad5fdcc1efa8ebe2662649f0070f330e8
Tatevik777 <67191920+Tatevik777@users.noreply.github.com>
2024-11-24 23:57:11 +0300

    week3 added

---
+++ src/java/by/tatevik/week3/Car.java
@@ -0,0 +1,21 @@
+package by.tatevik.week3;
+
+public class Car {
+    public String colour;
+    public String brand;
+
+    public Car(String colour, String brand){
+        this.colour=colour;
+        this.brand=brand;
+    }
+    public Car(){
+
+    }
+    public String goCar(){
+        return "Вжууххххх";
+    }
+    public String stopCar(){
+        return "Остановка";
+    }
+
+}
