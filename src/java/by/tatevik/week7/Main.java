f5f5908367c3b34c1678adae19a5595b88b4537d
Tatevik777 <67191920+Tatevik777@users.noreply.github.com>
2024-12-14 00:15:46 +0300

    week 7 update

--- src/java/by/tatevik/week7/Main.java
+++ src/java/by/tatevik/week7/Main.java
@@ -6,22 +6,23 @@ import java.util.Set;
 import java.util.Stack;
 
 public class Main {
-    private static final Set<Character> firstBrackets= Set.of('(','[','{');
-    private static final Map<Character,Character> accBrackets= Map.of(')','(','}','{',']','[');
+
+    private static final Set<Character> firstBrackets = Set.of('(', '[', '{');
+    private static final Map<Character, Character> accBrackets = Map.of(')', '(', '}', '{', ']', '[');
 
     public static void main(String[] args) {
-        int[]nums={0,0,1,1,2,2,3,4,4};
-        int[] expectedNums={0,1,2,3,4};
-        int[] result= removeDuplicates(nums);
-        System.out.println(Arrays.equals(result,expectedNums));
+
+        int[] nums = {0, 0, 1, 1, 2, 2, 3, 4, 4};
+        int[] expectedNums = {0, 1, 2, 3, 4};
+        int[] result = removeDuplicates(nums);
+        System.out.println(Arrays.equals(result, expectedNums));
         System.out.println(Arrays.toString(result));
 
 
-        String brackets= "{[()]}";
-        String brackets2= "([{)]}";
+        String brackets = "{[()]}";
+        String brackets2 = "([{)]}";
         System.out.println(isValid(brackets));
         System.out.println(isValid(brackets2));
-
     }
 
     public static int[] removeDuplicates(int[] nums) {
@@ -37,19 +38,16 @@ public class Main {
 
     public static boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        int length=s.length();
-        boolean isCorrect=Boolean.TRUE;
-        for(int i=0;i < length && isCorrect;i++) {
-            char ch=s.charAt(i);
-            if(firstBrackets.contains(ch)){
+        int length = s.length();
+        boolean isCorrect = Boolean.TRUE;
+        for (int i = 0; i < length && isCorrect; i++) {
+            char ch = s.charAt(i);
+            if (firstBrackets.contains(ch)) {
                 stack.push(ch);
-            } else if(!stack.isEmpty() && accBrackets.get(ch)!=stack.pop()){
-                isCorrect=Boolean.FALSE;
-
+            } else if (!stack.isEmpty() && accBrackets.get(ch) != stack.pop()) {
+                isCorrect = Boolean.FALSE;
             }
-            }
-        return  isCorrect;
         }
-
-
+        return isCorrect;
+    }
 }
